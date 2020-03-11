378b1d990df35520a40078c87400abe88146b4dd
Dario Izzo <dario.izzo@gmail.com>
2020-03-11 09:10:15 +0100

    building pybind from source attempt N.1

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,6 +7,16 @@ if [[ "$target_platform" == linux-64 ]]; then
     LDFLAGS="-lrt ${LDFLAGS}"
 fi
 
+# On OSX we install pybind from head as a workaround to the current relase not working on c++17.
+if [[ "$(uname)" == "Darwin" ]]; then
+    git clone https://github.com/pybind/pybind11.git
+    cd pybind11
+    mkdir build
+    cd build
+    cmake .. -DPYBIND11_TEST=NO \
+    make install
+fi
+
 cmake \
     -DBoost_NO_BOOST_CMAKE=ON \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
