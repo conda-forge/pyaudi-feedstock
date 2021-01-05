8b005e769b0b5a6cb754a1619068cabb492aa93c
Dario Izzo <dario.izzo@gmail.com>
2021-01-05 13:10:16 +0100

    removed pybind11 pin

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,21 +2,6 @@ mkdir build
 cd build
 SET PYAUDI_BUILD_DIR=%cd%
 
-git clone https://github.com/pybind/pybind11.git
-cd pybind11
-git checkout 4f72ef846fe8453596230ac285eeaa0ce3278bb4
-mkdir build
-cd build
-cmake ^
-    -G "Ninja" ^
-    -DPYBIND11_TEST=NO ^
-    -DCMAKE_INSTALL_PREFIX=%PYAUDI_BUILD_DIR% ^
-    -DCMAKE_PREFIX_PATH=%PYAUDI_BUILD_DIR% ^
-    -DCMAKE_BUILD_TYPE=Release ^
-    ..
-cmake --build . --target install
-cd ../..
-
 cmake ^
     -G "Ninja" ^
     -DCMAKE_C_COMPILER=clang-cl ^
@@ -27,7 +12,6 @@ cmake ^
     -DAUDI_BUILD_TESTS=no ^
     -DAUDI_BUILD_AUDI=no ^
     -DAUDI_BUILD_PYAUDI=yes ^
-    -Dpybind11_DIR=%PYAUDI_BUILD_DIR%\share\cmake\pybind11\ ^
     -DCMAKE_BUILD_TYPE=Release ^
     ..
 
